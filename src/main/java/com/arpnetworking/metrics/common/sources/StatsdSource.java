d3a06ff2689e752f3b1c2686ca21cf025213e9c4
Ville Koskela <ville@koskelafamily.com>
2017-08-29 20:52:11 -0700

    Add statsd protocol compatible udp source. (#62)
    * Add statsd protocol compatible udp source.

---
+++ src/main/java/com/arpnetworking/metrics/common/sources/StatsdSource.java
@@ -0,0 +1,201 @@
+/**
+ * Copyright 2017 Inscope Metrics, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.arpnetworking.metrics.common.sources;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import akka.io.Udp;
+import akka.io.UdpMessage;
+import com.arpnetworking.metrics.common.parsers.Parser;
+import com.arpnetworking.metrics.mad.model.Record;
+import com.arpnetworking.metrics.mad.parsers.StatsdToRecordParser;
+import com.arpnetworking.steno.Logger;
+import com.arpnetworking.steno.LoggerFactory;
+import jdk.nashorn.internal.runtime.ParserException;
+import net.sf.oval.constraint.NotEmpty;
+import net.sf.oval.constraint.NotNull;
+import net.sf.oval.constraint.Range;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.time.Duration;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Source that uses Statsd as input.
+ *
+ * @author Ville Koskela (ville dot koskela at inscopemetrics dot com)
+ */
+public final class StatsdSource extends ActorSource {
+
+    @Override
+    protected Props createProps() {
+        return Actor.props(this);
+    }
+
+    /**
+     * Protected constructor.
+     *
+     * @param builder Instance of <code>Builder</code>.
+     */
+    private StatsdSource(final Builder builder) {
+        super(builder);
+        _host = builder._host;
+        _port = builder._port;
+    }
+
+    private final String _host;
+    private final int _port;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StatsdSource.class);
+    private static final Parser<List<Record>, ByteBuffer> PARSER = new StatsdToRecordParser();
+
+    /**
+     * Internal actor to process requests.
+     */
+    /* package private */ static final class Actor extends UntypedActor {
+        /**
+         * Creates a {@link Props} for this actor.
+         *
+         * @param source The {@link StatsdSource} to send notifications through.
+         * @return A new {@link Props}
+         */
+        /* package private */ static Props props(final StatsdSource source) {
+            return Props.create(Actor.class, source);
+        }
+
+        @Override
+        public void onReceive(final Object message) throws Exception {
+            if (Objects.equals(IS_READY, message)) {
+                getSender().tell(_isReady, getSelf());
+            } else if (message instanceof Udp.Bound) {
+                final Udp.Bound updBound = (Udp.Bound) message;
+                LOGGER.debug()
+                        .setMessage("Statsd server binding complete")
+                        .addData("address", updBound.localAddress().getAddress().getHostAddress())
+                        .addData("port", updBound.localAddress().getPort())
+                        .log();
+                _socket = getSender();
+                _isReady = true;
+            } else if (message instanceof Udp.Received) {
+                final Udp.Received updReceived = (Udp.Received) message;
+                LOGGER.trace()
+                        .setMessage("Statsd received datagram")
+                        .addData("bytes", updReceived.data().size())
+                        .log();
+
+                try {
+                    // NOTE: The parsing occurs in the actor itself which can become a bottleneck
+                    // if there are more records to be parsed then a single thread can handle.
+                    final List<Record> records = PARSER.parse(updReceived.data().toByteBuffer());
+                    records.forEach(_sink::notify);
+                } catch (final ParserException e) {
+                    BAD_REQUEST_LOGGER.warn()
+                            .setMessage("Error handling statsd datagram")
+                            .setThrowable(e)
+                            .log();
+                }
+
+            } else if (Objects.equals(message, UdpMessage.unbind())) {
+                _socket.tell(message, getSelf());
+
+            } else if (message instanceof Udp.Unbound) {
+                getContext().stop(getSelf());
+
+            } else {
+                unhandled(message);
+            }
+        }
+
+        /**
+         * Constructor.
+         *
+         * @param source The {@link StatsdSource} to send notifications through.
+         */
+        /* package private */ Actor(final StatsdSource source) {
+            _sink = source;
+            _host = source._host;
+            _port = source._port;
+
+            final ActorRef udpManager = Udp.get(getContext().system()).getManager();
+            udpManager.tell(
+                    UdpMessage.bind(getSelf(), new InetSocketAddress(_host, _port)),
+                    getSelf());
+        }
+
+        private boolean _isReady = false;
+        private ActorRef _socket;
+        private final StatsdSource _sink;
+        private final String _host;
+        private final int _port;
+
+        private static final String IS_READY = "IsReady";
+        private static final Logger BAD_REQUEST_LOGGER =
+                LoggerFactory.getRateLimitLogger(StatsdSource.class, Duration.ofSeconds(30));
+    }
+
+    /**
+     * StatsdSource {@link BaseSource.Builder} implementation.
+     *
+     * @author Ville Koskela (ville dot koskela at inscopemetrics dot com)
+     */
+    public static final class Builder extends ActorSource.Builder<Builder, StatsdSource> {
+
+        /**
+         * Public constructor.
+         */
+        public Builder() {
+            super(StatsdSource::new);
+        }
+
+        /**
+         * Sets the host to bind to. Optional. Cannot be null or empty.
+         *
+         * @param value the port to listen on
+         * @return This builder
+         */
+        public Builder setHost(final String value) {
+            _host = value;
+            return self();
+        }
+
+        /**
+         * Sets the port to listen on. Optional. Cannot be null. Must be
+         * between 1 and 65535 (inclusive). Default is 8125.
+         *
+         * @param value the port to listen on
+         * @return This builder
+         */
+        public Builder setPort(final Integer value) {
+            _port = value;
+            return self();
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+
+        @NotNull
+        @NotEmpty
+        private String _host = "localhost";
+        @NotNull
+        @Range(min = 1, max = 65535)
+        private Integer _port = 8125;
+    }
+}
---
+++ src/test/java/com/arpnetworking/metrics/mad/parsers/StatsdToRecordParserTest.java
@@ -0,0 +1,241 @@
+/**
+ * Copyright 2017 Inscope Metrics, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.arpnetworking.metrics.mad.parsers;
+
+import com.arpnetworking.metrics.common.parsers.Parser;
+import com.arpnetworking.metrics.common.parsers.exceptions.ParsingException;
+import com.arpnetworking.metrics.mad.model.DefaultMetric;
+import com.arpnetworking.metrics.mad.model.DefaultRecord;
+import com.arpnetworking.metrics.mad.model.Record;
+import com.arpnetworking.tsdcore.model.MetricType;
+import com.arpnetworking.tsdcore.model.Quantity;
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.nio.ByteBuffer;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+/**
+ * Tests for the statsd parser.
+ *
+ * @author Ville Koskela (ville dot koskela at inscopemetrics dot com)
+ */
+public final class StatsdToRecordParserTest {
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testExampleCounter() throws ParsingException {
+        assertRecordEquality(
+                new DefaultRecord.Builder()
+                        .setTime(_now)
+                        .setId(UUID.randomUUID().toString())
+                        .setAnnotations(ImmutableMap.of())
+                        .setDimensions(ImmutableMap.of())
+                        .setMetrics(ImmutableMap.of(
+                                "page.views",
+                                new DefaultMetric.Builder()
+                                        .setType(MetricType.COUNTER)
+                                        .setValues(Collections.singletonList(
+                                                new Quantity.Builder()
+                                                        .setValue(1.0)
+                                                        .build()))
+                                        .build()
+                        ))
+                        .build(),
+                Iterables.getOnlyElement(
+                        _parser.parse(ByteBuffer.wrap("page.views:1|c".getBytes(Charsets.UTF_8)))));
+    }
+
+    @Test
+    public void testExampleGauge() throws ParsingException {
+        assertRecordEquality(
+                new DefaultRecord.Builder()
+                        .setTime(_now)
+                        .setId(UUID.randomUUID().toString())
+                        .setAnnotations(ImmutableMap.of())
+                        .setDimensions(ImmutableMap.of())
+                        .setMetrics(ImmutableMap.of(
+                                "fuel.level",
+                                new DefaultMetric.Builder()
+                                        .setType(MetricType.GAUGE)
+                                        .setValues(Collections.singletonList(
+                                                new Quantity.Builder()
+                                                        .setValue(0.5)
+                                                        .build()))
+                                        .build()
+                        ))
+                        .build(),
+                Iterables.getOnlyElement(
+                        _parser.parse(ByteBuffer.wrap("fuel.level:0.5|g".getBytes(Charsets.UTF_8)))));
+    }
+
+    @Test
+    public void testExampleSamplingAccept() throws ParsingException {
+        Mockito.doReturn(0.49).when(_random).nextDouble();
+        assertRecordEquality(
+                new DefaultRecord.Builder()
+                        .setTime(_now)
+                        .setId(UUID.randomUUID().toString())
+                        .setAnnotations(ImmutableMap.of())
+                        .setDimensions(ImmutableMap.of())
+                        .setMetrics(ImmutableMap.of(
+                                "song.length",
+                                new DefaultMetric.Builder()
+                                        .setType(MetricType.TIMER)
+                                        .setValues(Collections.singletonList(
+                                                new Quantity.Builder()
+                                                        .setValue(240.0)
+                                                        .build()))
+                                        .build()
+                        ))
+                        .build(),
+                Iterables.getOnlyElement(
+                        _parser.parse(ByteBuffer.wrap("song.length:240|h|@0.5".getBytes(Charsets.UTF_8)))));
+    }
+
+    @Test
+    public void testExampleSamplingReject() throws ParsingException {
+        Mockito.doReturn(0.51).when(_random).nextDouble();
+        Assert.assertTrue(_parser.parse(ByteBuffer.wrap("song.length:240|h|@0.5".getBytes(Charsets.UTF_8))).isEmpty());
+    }
+
+    @Test
+    public void testSamplingAlwaysAccept() throws ParsingException {
+        Mockito.doReturn(0.0).when(_random).nextDouble();
+        assertRecordEquality(
+                new DefaultRecord.Builder()
+                        .setTime(_now)
+                        .setId(UUID.randomUUID().toString())
+                        .setAnnotations(ImmutableMap.of())
+                        .setDimensions(ImmutableMap.of())
+                        .setMetrics(ImmutableMap.of(
+                                "song.length",
+                                new DefaultMetric.Builder()
+                                        .setType(MetricType.TIMER)
+                                        .setValues(Collections.singletonList(
+                                                new Quantity.Builder()
+                                                        .setValue(240.0)
+                                                        .build()))
+                                        .build()
+                        ))
+                        .build(),
+                Iterables.getOnlyElement(
+                        _parser.parse(ByteBuffer.wrap("song.length:240|h|@1".getBytes(Charsets.UTF_8)))));
+    }
+
+    @Test
+    public void testSamplingAlwaysReject() throws ParsingException {
+        Mockito.doReturn(0.0).when(_random).nextDouble();
+        Assert.assertTrue(_parser.parse(ByteBuffer.wrap("song.length:240|h|@0".getBytes(Charsets.UTF_8))).isEmpty());
+    }
+
+    @Test(expected = ParsingException.class)
+    public void testExampleSetsNotSupported() throws ParsingException {
+        _parser.parse(ByteBuffer.wrap("users.uniques:1234|s".getBytes(Charsets.UTF_8)));
+    }
+
+    @Test
+    public void testExampleTags() throws ParsingException {
+        assertRecordEquality(
+                new DefaultRecord.Builder()
+                        .setTime(_now)
+                        .setId(UUID.randomUUID().toString())
+                        .setAnnotations(ImmutableMap.of())
+                        .setDimensions(ImmutableMap.of(
+                                "country", "china"))
+                        .setMetrics(ImmutableMap.of(
+                                "users.online",
+                                new DefaultMetric.Builder()
+                                        .setType(MetricType.COUNTER)
+                                        .setValues(Collections.singletonList(
+                                                new Quantity.Builder()
+                                                        .setValue(1.0)
+                                                        .build()))
+                                        .build()
+                        ))
+                        .build(),
+                Iterables.getOnlyElement(
+                        _parser.parse(ByteBuffer.wrap("users.online:1|c|#country:china".getBytes(Charsets.UTF_8)))));
+    }
+
+    @Test
+    public void testExampleTagsSampledAccept() throws ParsingException {
+        Mockito.doReturn(0.49).when(_random).nextDouble();
+        assertRecordEquality(
+                new DefaultRecord.Builder()
+                        .setTime(_now)
+                        .setId(UUID.randomUUID().toString())
+                        .setAnnotations(ImmutableMap.of())
+                        .setDimensions(ImmutableMap.of(
+                                "country", "china"))
+                        .setMetrics(ImmutableMap.of(
+                                "users.online",
+                                new DefaultMetric.Builder()
+                                        .setType(MetricType.COUNTER)
+                                        .setValues(Collections.singletonList(
+                                                new Quantity.Builder()
+                                                        .setValue(1.0)
+                                                        .build()))
+                                        .build()
+                        ))
+                        .build(),
+                Iterables.getOnlyElement(
+                        _parser.parse(ByteBuffer.wrap("users.online:1|c|@0.5|#country:china".getBytes(Charsets.UTF_8)))));
+    }
+
+    @Test
+    public void testExampleTagsSampledReject() throws ParsingException {
+        Mockito.doReturn(0.51).when(_random).nextDouble();
+        Assert.assertTrue(_parser.parse(ByteBuffer.wrap("users.online:1|c|@0.5|#country:china".getBytes(Charsets.UTF_8))).isEmpty());
+    }
+
+    private void assertRecordEquality(final Record expected, final Record actual) {
+        Assert.assertEquals(expected.getTime(), actual.getTime());
+        Assert.assertEquals(expected.getAnnotations(), actual.getAnnotations());
+        Assert.assertEquals(expected.getDimensions(), actual.getDimensions());
+        Assert.assertEquals(expected.getMetrics(), actual.getMetrics());
+    }
+
+    @Mock
+    private Random _random;
+
+    private final DateTime _now = DateTime.now(DateTimeZone.UTC);
+    private final Parser<List<Record>, ByteBuffer> _parser =
+            new StatsdToRecordParser(
+                    Clock.fixed(
+                            Instant.ofEpochMilli(_now.getMillis()),
+                            ZoneId.of("UTC")),
+                    () -> _random);
+}
