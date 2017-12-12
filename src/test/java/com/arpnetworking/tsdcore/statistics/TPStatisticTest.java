bd18be27d24f468358be040f853ad69cb7a3a966
ddimensia <ddimensia@gmail.com>
2017-12-12 11:03:39 -0800

    Adds P75 Statistic to list of available percentile statistics. (#71)
    Added support for a 75th Percentile statistic

--- src/test/java/com/arpnetworking/tsdcore/statistics/TPStatisticTest.java
+++ src/test/java/com/arpnetworking/tsdcore/statistics/TPStatisticTest.java
@@ -34,6 +34,7 @@ public class TPStatisticTest {
 
     @Test
     public void testName() {
+        Assert.assertEquals("tp75", TP75_STATISTIC.getName());
         Assert.assertEquals("tp90", TP90_STATISTIC.getName());
         Assert.assertEquals("tp95", TP95_STATISTIC.getName());
         Assert.assertEquals("tp99", TP99_STATISTIC.getName());
@@ -42,6 +43,10 @@ public class TPStatisticTest {
 
     @Test
     public void testEquality() {
+        Assert.assertFalse(TP75_STATISTIC.equals(null));
+        Assert.assertFalse(TP75_STATISTIC.equals("ABC"));
+        Assert.assertTrue(TP75_STATISTIC.equals(TP75_STATISTIC));
+
         Assert.assertFalse(TP90_STATISTIC.equals(null));
         Assert.assertFalse(TP90_STATISTIC.equals("ABC"));
         Assert.assertTrue(TP90_STATISTIC.equals(TP90_STATISTIC));
@@ -61,10 +66,22 @@ public class TPStatisticTest {
 
     @Test
     public void testHashCode() {
+        Assert.assertEquals(TP75_STATISTIC.hashCode(), TP75_STATISTIC.hashCode());
         Assert.assertEquals(TP90_STATISTIC.hashCode(), TP90_STATISTIC.hashCode());
         Assert.assertEquals(TP95_STATISTIC.hashCode(), TP95_STATISTIC.hashCode());
         Assert.assertEquals(TP99_STATISTIC.hashCode(), TP99_STATISTIC.hashCode());
         Assert.assertEquals(TP99P9_STATISTIC.hashCode(), TP99P9_STATISTIC.hashCode());
+    }
+
+    @Test
+    public void testTP75Accumulator() {
+        final Accumulator<?> accumulator = (Accumulator<?>) HISTOGRAM_STATISTIC.createCalculator();
+        for (int x = 1; x <= 10000; ++x) {
+            accumulator.accumulate(new Quantity.Builder().setValue((double) x).build());
+        }
+        final CalculatedValue<Void> calculated = TP75_STATISTIC.createCalculator().calculate(
+                Collections.singletonMap(HISTOGRAM_STATISTIC, accumulator));
+        Assert.assertTrue(areClose(new Quantity.Builder().setValue(7500.0).build(), calculated.getValue()));
     }
 
     @Test
@@ -132,6 +149,7 @@ public class TPStatisticTest {
     private static final List<Double> ONE_TO_FIVE = Lists.newArrayList(1d, 2d, 3d, 4d, 5d);
     private static final StatisticFactory STATISTIC_FACTORY = new StatisticFactory();
     private static final Statistic HISTOGRAM_STATISTIC = STATISTIC_FACTORY.getStatistic("histogram");
+    private static final TP75Statistic TP75_STATISTIC = (TP75Statistic) STATISTIC_FACTORY.getStatistic("tp75");
     private static final TP90Statistic TP90_STATISTIC = (TP90Statistic) STATISTIC_FACTORY.getStatistic("tp90");
     private static final TP95Statistic TP95_STATISTIC = (TP95Statistic) STATISTIC_FACTORY.getStatistic("tp95");
     private static final TP99Statistic TP99_STATISTIC = (TP99Statistic) STATISTIC_FACTORY.getStatistic("tp99");
