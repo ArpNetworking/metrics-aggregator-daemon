c4ee0191993311b540ba15a2d6aa8c722a2005d8
Gilligan Markham <gmarkham@dropbox.com>
2018-05-30 15:21:08 -0700

    Added 'set -e' to cause the scripts to fail on the first non-zero exit status rather than allow the failures to cascade.

--- rpm/scripts/postun.sh
+++ rpm/scripts/postun.sh
@@ -1,3 +1,4 @@
+set -e
 if [ "$1" -ge 1 ]; then
   /sbin/service mad condrestart > /dev/null 2>&1
 fi
--- rpm/scripts/preun.sh
+++ rpm/scripts/preun.sh
@@ -1,3 +1,4 @@
+set -e
 if [ "$1" = 0 ]; then
   /sbin/service mad stop > /dev/null 2>&1
   /sbin/chkconfig --del mad
