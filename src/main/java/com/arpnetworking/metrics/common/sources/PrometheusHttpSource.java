01253c4a408dfa673ed253b0f1bdf48028f4d167
Bruno Green <bruno.green@gmail.com>
2018-10-24 06:50:53 +0000

    checkstyle

--- src/main/java/com/arpnetworking/metrics/common/sources/PrometheusHttpSource.java
+++ src/main/java/com/arpnetworking/metrics/common/sources/PrometheusHttpSource.java
@@ -1,14 +1,34 @@
+/*
+ * Copyright 2018 Inscope Metrics, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.arpnetworking.metrics.common.sources;
 
 import com.arpnetworking.metrics.mad.parsers.PrometheusToRecordParser;
 
+/**
+ * Processes Prometheus messages, extracts data and emits metrics.
+ *
+ * @author Bruno Green (bruno dot green at gmail dot com)
+ */
 public final class PrometheusHttpSource extends HttpSource{
     /**
      * Protected constructor.
      *
      * @param builder Instance of <code>Builder</code>.
      */
-    protected PrometheusHttpSource(HttpSource.Builder<?, ? extends HttpSource> builder) {
+    private PrometheusHttpSource(final HttpSource.Builder<?, ? extends HttpSource> builder) {
         super(builder);
     }
 
--- src/main/java/com/arpnetworking/metrics/mad/parsers/PrometheusToRecordParser.java
+++ src/main/java/com/arpnetworking/metrics/mad/parsers/PrometheusToRecordParser.java
@@ -1,9 +1,28 @@
+/*
+ * Copyright 2018 Inscope Metrics, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.arpnetworking.metrics.mad.parsers;
 
 import com.arpnetworking.commons.builder.ThreadLocalBuilder;
 import com.arpnetworking.metrics.common.parsers.Parser;
 import com.arpnetworking.metrics.common.parsers.exceptions.ParsingException;
-import com.arpnetworking.metrics.mad.model.*;
+import com.arpnetworking.metrics.mad.model.DefaultMetric;
+import com.arpnetworking.metrics.mad.model.DefaultRecord;
+import com.arpnetworking.metrics.mad.model.HttpRequest;
+import com.arpnetworking.metrics.mad.model.Metric;
+import com.arpnetworking.metrics.mad.model.Record;
 import com.arpnetworking.metrics.prometheus.Remote;
 import com.arpnetworking.metrics.prometheus.Types;
 import com.arpnetworking.metrics.prometheus.Types.TimeSeries;
@@ -25,6 +44,11 @@ import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Parses the Prometheus protobuf binary protocol into records.
+ *
+ * @author Bruno Green (bruno dot green at gmail dot com)
+ */
 public class PrometheusToRecordParser implements Parser<List<Record>, HttpRequest> {
     private static final ImmutableMap<String, Unit> UNIT_MAP = ImmutableMap.of(
             createUnitMapKey("seconds"), Unit.SECOND,
@@ -40,19 +64,19 @@ public class PrometheusToRecordParser implements Parser<List<Record>, HttpReques
             createUnitMapKey("count")
     );
 
-    private static String createUnitMapKey(String name) {
+    private static String createUnitMapKey(final String name) {
         return new StringBuilder(name).reverse().toString();
     }
 
     private Unit extractUnit(final String name) {
         if (name != null) {
-            StringBuilder builder = new StringBuilder();
+            final StringBuilder builder = new StringBuilder();
             for (int i = name.length() - 1; i >= 0; i--) {
-                char ch = name.charAt(i);
+                final char ch = name.charAt(i);
                 if (ch == '_') {
                     final String key = builder.toString();
                     if (PROMETHEUS_AGGREGATION_KEYS.contains(key)) {
-                        builder.setLength(0);//reset builder
+                        builder.setLength(0); //reset builder
                     } else {
                         return UNIT_MAP.get(key);
                     }
@@ -65,25 +89,26 @@ public class PrometheusToRecordParser implements Parser<List<Record>, HttpReques
     }
 
     @Override
-    public List<Record> parse(HttpRequest data) throws ParsingException {
+    public List<Record> parse(final HttpRequest data) throws ParsingException {
         final List<Record> records = Lists.newArrayList();
         try {
             final byte[] uncompressed = Snappy.uncompress(data.getBody().toArray());
             final Remote.WriteRequest writeRequest = Remote.WriteRequest.parseFrom(uncompressed);
             for (final TimeSeries timeSeries : writeRequest.getTimeseriesList()) {
                 String name = null;
-                ImmutableMap.Builder<String, String> dimensionsBuilder = ImmutableMap.builder();
+                final ImmutableMap.Builder<String, String> dimensionsBuilder = ImmutableMap.builder();
                 for (Types.Label label : timeSeries.getLabelsList()) {
-                    if (label.getName().equals("__name__"))
+                    if (label.getName().equals("__name__")) {
                         name = label.getValue();
-                    else
+                    } else {
                         dimensionsBuilder.put(label.getName(), label.getValue());
+                    }
                 }
                 final ImmutableMap<String, String> immutableDimensions = dimensionsBuilder.build();
                 final String metricName = name;
                 final Unit unit = extractUnit(name);
                 for (final Types.Sample sample : timeSeries.getSamplesList()) {
-                    Record record = ThreadLocalBuilder.build(
+                    final Record record = ThreadLocalBuilder.build(
                             DefaultRecord.Builder.class,
                             b -> b.setId(UUID.randomUUID().toString())
                                     .setTime(
@@ -107,8 +132,7 @@ public class PrometheusToRecordParser implements Parser<List<Record>, HttpReques
     }
 
     private ImmutableMap<String, ? extends Metric> createMetric(final String name, final Types.Sample sample, final Unit unit) {
-
-        Metric metric = ThreadLocalBuilder.build(
+        final Metric metric = ThreadLocalBuilder.build(
                 DefaultMetric.Builder.class,
                 p -> p
                         .setType(MetricType.GAUGE)
