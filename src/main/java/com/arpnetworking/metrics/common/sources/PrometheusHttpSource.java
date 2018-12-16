029059c4aadf1a2c8eb119f9eb9dc233d10a6cdc
Bruno Green <bruno.green@gmail.com>
2018-12-16 21:54:19 +0000

    copyright

--- src/main/java/com/arpnetworking/metrics/common/sources/PrometheusHttpSource.java
+++ src/main/java/com/arpnetworking/metrics/common/sources/PrometheusHttpSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Inscope Metrics, Inc.
+ * Copyright 2018 Bruno Green.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
