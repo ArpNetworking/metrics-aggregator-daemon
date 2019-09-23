8bdfa7b99f67010598cdf002e7b8285961cd746b
Ville Koskela <vkoskela@dropbox.com>
2019-09-23 16:02:49 -0700

    Pre-aggregation integration test.

--- src/test/java/com/arpnetworking/metrics/mad/integration/TelemetryIT.java
+++ src/test/java/com/arpnetworking/metrics/mad/integration/TelemetryIT.java
@@ -17,10 +17,13 @@ package com.arpnetworking.metrics.mad.integration;
 
 import com.arpnetworking.metrics.Metrics;
 import com.arpnetworking.metrics.MetricsFactory;
+import com.arpnetworking.metrics.impl.AugmentedHistogram;
+import com.arpnetworking.metrics.impl.TsdMetrics;
 import com.arpnetworking.metrics.impl.TsdMetricsFactory;
 import com.arpnetworking.metrics.mad.model.statistics.Statistic;
 import com.arpnetworking.metrics.mad.model.statistics.StatisticFactory;
 import com.arpnetworking.test.TelemetryClient;
+import com.google.common.collect.ImmutableMap;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -43,8 +46,8 @@ import java.util.concurrent.TimeoutException;
 @RunWith(Parameterized.class)
 public final class TelemetryIT {
 
-    public TelemetryIT(final Statistic statistic, final double expectedResult) {
-        _statistic = statistic;
+    public TelemetryIT(final String statisticName, final double expectedResult) {
+        _statistic = STATISTIC_FACTORY.getStatistic(statisticName);
         _expectedResult = expectedResult;
         _telemetryClient = TelemetryClient.getInstance();
     }
@@ -57,15 +60,15 @@ public final class TelemetryIT {
     @Parameterized.Parameters(name = "{0}")
     public static Collection<Object[]> createParameters() {
         return Arrays.asList(
-                new Object[]{STATISTIC_FACTORY.getStatistic("count"), 55.0d},
-                new Object[]{STATISTIC_FACTORY.getStatistic("sum"), 423.5d},
-                new Object[]{STATISTIC_FACTORY.getStatistic("mean"), 7.7d},
-                new Object[]{STATISTIC_FACTORY.getStatistic("max"), 11.0d},
-                new Object[]{STATISTIC_FACTORY.getStatistic("min"), 1.1d});
+                new Object[]{"count", 55.0d},
+                new Object[]{"sum", 423.5d},
+                new Object[]{"mean", 7.7d},
+                new Object[]{"max", 11.0d},
+                new Object[]{"min", 1.1d});
                 // TODO(ville): enable with mad-2.0
-                //new Object[]{STATISTIC_FACTORY.getStatistic("median"), 7.7d},
-                //new Object[]{STATISTIC_FACTORY.getStatistic("p25"), 5.5d},
-                //new Object[]{STATISTIC_FACTORY.getStatistic("p75"), 9.9d});
+                //new Object[]{"median", 7.7d},
+                //new Object[]{"p25", 5.5d},
+                //new Object[]{"p75", 9.9d});
     }
 
     @Test
@@ -93,6 +96,141 @@ public final class TelemetryIT {
                     "TelemetryIT",
                     _metricName,
                     _statistic);
+        }
+    }
+
+
+    @Test
+    public void testFromAggregatedData() throws InterruptedException, ExecutionException, TimeoutException {
+        final CompletableFuture<Double> future = new CompletableFuture<>();
+
+        try {
+            _telemetryClient.subscribe(
+                    "TelemetryIT",
+                    _metricName,
+                    _statistic,
+                    future::complete);
+
+            sleepToBeginningOfSecond();
+
+            try (Metrics metrics = METRICS_FACTORY.create()) {
+                final TsdMetrics tsdMetrics = (TsdMetrics) metrics;
+                tsdMetrics.recordAggregatedData(
+                        _metricName,
+                        new AugmentedHistogram.Builder()
+                                .setMinimum(1.1)
+                                .setMaximum(5.5)
+                                .setSum(60.5)
+                                .setHistogram(ImmutableMap.of(
+                                        1.1, 1,
+                                        2.2, 2,
+                                        3.3, 3,
+                                        4.4, 4,
+                                        5.5, 5))
+                                .setPrecision(7)
+                                .build());
+            }
+            try (Metrics metrics = METRICS_FACTORY.create()) {
+                final TsdMetrics tsdMetrics = (TsdMetrics) metrics;
+                tsdMetrics.recordAggregatedData(
+                        _metricName,
+                        new AugmentedHistogram.Builder()
+                                .setMinimum(6.6)
+                                .setMaximum(11.0)
+                                .setSum(363.0)
+                                .setHistogram(ImmutableMap.of(
+                                        6.6, 6,
+                                        7.7, 7,
+                                        8.8, 8,
+                                        9.9, 9,
+                                        11.0, 10))
+                                .setPrecision(7)
+                                .build());
+            }
+
+            Assert.assertEquals(_expectedResult, future.get(5, TimeUnit.SECONDS), 0.0001);
+        } finally {
+            _telemetryClient.unsubscribe(
+                    "TelemetryIT",
+                    _metricName,
+                    _statistic);
+        }
+    }
+
+    @Test
+    public void testFromMixedSamplesAndAggregatedData() throws InterruptedException, ExecutionException, TimeoutException {
+        final CompletableFuture<Double> future = new CompletableFuture<>();
+
+        try {
+            _telemetryClient.subscribe(
+                    "TelemetryIT",
+                    _metricName,
+                    _statistic,
+                    future::complete);
+
+            sleepToBeginningOfSecond();
+
+            // First mixed samples-aggregates unit of work
+            try (Metrics metrics = METRICS_FACTORY.create()) {
+                for (int i = 1; i <= 2; ++i) {
+                    for (int j = 1; j <= i; ++j) {
+                        metrics.setGauge(_metricName, i * 1.1d);
+                    }
+                }
+
+                final TsdMetrics tsdMetrics = (TsdMetrics) metrics;
+                tsdMetrics.recordAggregatedData(
+                        _metricName,
+                        new AugmentedHistogram.Builder()
+                                .setMinimum(3.3)
+                                .setMaximum(5.5)
+                                .setSum(55.0)
+                                .setHistogram(ImmutableMap.of(
+                                        3.3, 3,
+                                        4.4, 4,
+                                        5.5, 5))
+                                .setPrecision(7)
+                                .build());
+            }
+
+            // Second mixed samples-aggregates unit of work
+            try (Metrics metrics = METRICS_FACTORY.create()) {
+                for (int i = 6; i <= 8; ++i) {
+                    for (int j = 1; j <= i; ++j) {
+                        metrics.setGauge(_metricName, i * 1.1d);
+                    }
+                }
+
+                final TsdMetrics tsdMetrics = (TsdMetrics) metrics;
+                tsdMetrics.recordAggregatedData(
+                        _metricName,
+                        new AugmentedHistogram.Builder()
+                                .setMinimum(9.9)
+                                .setMaximum(11.0)
+                                .setSum(199.1)
+                                .setHistogram(ImmutableMap.of(
+                                        9.9, 9,
+                                        11.0, 10))
+                                .setPrecision(7)
+                                .build());
+            }
+
+            Assert.assertEquals(_expectedResult, future.get(5, TimeUnit.SECONDS), 0.0001);
+        } finally {
+            _telemetryClient.unsubscribe(
+                    "TelemetryIT",
+                    _metricName,
+                    _statistic);
+        }
+    }
+
+    private static void sleepToBeginningOfSecond() {
+        while (System.currentTimeMillis() % 1000 >= 100) {
+            try {
+                Thread.sleep(1);
+            } catch (final InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
