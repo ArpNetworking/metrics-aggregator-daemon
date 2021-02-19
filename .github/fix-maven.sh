df0482824f0e2250323f5a693f1462b48df58902
Ville Koskela <ville@koskelafamily.com>
2021-02-19 12:06:46 -0800

    Fix Maven on GHA by overriding the http wagon with a custom build.

---
+++ .github/fix-maven.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+MAVEN_HOME=$(./jdk-wrapper.sh ./mvnw -v |grep 'Maven home:' | awk '{ print $3 }')
+if [ -d "$MAVEN_HOME" ]; then
+  echo "Installing custom wagon-http jar to $MAVEN_HOME/lib"
+  ls -l "$MAVEN_HOME/lib/"wagon-http-*-shaded.jar
+  rm "$MAVEN_HOME/lib/"wagon-http-*-shaded.jar
+  cp ./.github/wagon-http-* "$MAVEN_HOME/lib/"
+  ls -l "$MAVEN_HOME/lib/"wagon-http-*-shaded.jar
+else
+  echo "Unable to fix Maven!"
+fi
