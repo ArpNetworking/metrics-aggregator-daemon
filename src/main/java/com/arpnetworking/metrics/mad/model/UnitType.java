dfd1b500551522b857ee71db442e397a69d2b619
Brandon Arp <brandonarp@gmail.com>
2023-06-05 15:11:18 -0700

    add javadoc

--- src/main/java/com/arpnetworking/metrics/mad/model/UnitType.java
+++ src/main/java/com/arpnetworking/metrics/mad/model/UnitType.java
@@ -22,18 +22,27 @@ package com.arpnetworking.metrics.mad.model;
  * @author Ville Koskela (ville dot koskela at inscopemetrics dot io)
  */
 public enum UnitType {
+    /**
+     * Used in describing time durations.
+     */
     TIME {
         @Override
         public Unit getDefaultUnit() {
             return Unit.SECOND;
         }
     },
+    /**
+     * Used in describing data sizes.
+     */
     DATA_SIZE {
         @Override
         public Unit getDefaultUnit() {
             return Unit.BYTE;
         }
     },
+    /**
+     * Used in describing measurements of temperature.
+     */
     TEMPERATURE {
         @Override
         public Unit getDefaultUnit() {
@@ -41,5 +50,10 @@ public enum UnitType {
         }
     };
 
+    /**
+     * Get the default unit for this unit type. Used in normalizing values.
+     *
+     * @return The default unit for a given domain.
+     */
     public abstract Unit getDefaultUnit();
 }
