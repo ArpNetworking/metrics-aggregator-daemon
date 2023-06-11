49317de4059c9553c42cae15988a1c46a6a64aab
Brandon Arp <brandonarp@gmail.com>
2023-06-11 13:52:57 -0700

    convert to strict instead

--- src/main/java/com/arpnetworking/http/Routes.java
+++ src/main/java/com/arpnetworking/http/Routes.java
@@ -107,17 +107,15 @@ public final class Routes implements Function<HttpRequest, CompletionStage<HttpR
     public CompletionStage<HttpResponse> apply(final HttpRequest request) {
         final Stopwatch requestTimer = Stopwatch.createStarted();
         final UUID requestId = UUID.randomUUID();
-        final CountingEntityWrapper entityWrapper = new CountingEntityWrapper((akka.http.scaladsl.model.RequestEntity) request.entity());
-        final HttpRequest wrappedRequest = request.withEntity(entityWrapper);
         if (LOGGER.isTraceEnabled()) {
             LOGGER.trace()
                     .setEvent("http.in.start")
                     .addContext("requestId", requestId)
-                    .addData("method", wrappedRequest.method().toString())
-                    .addData("url", wrappedRequest.getUri().toString())
+                    .addData("method", request.method().toString())
+                    .addData("url", request.getUri().toString())
                     .addData(
                             "headers",
-                            StreamSupport.stream(wrappedRequest.getHeaders().spliterator(), false)
+                            StreamSupport.stream(request.getHeaders().spliterator(), false)
                                     .map(h -> h.name() + "=" + h.value())
                                     .collect(Collectors.toList()))
                     .log();
@@ -125,57 +123,63 @@ public final class Routes implements Function<HttpRequest, CompletionStage<HttpR
         // TODO(ville): Add a request UUID and include in MDC.
         LOGGER.trace()
                 .setEvent("http.in.start")
-                .addData("method", wrappedRequest.method())
-                .addData("url", wrappedRequest.getUri())
-                .addData("headers", wrappedRequest.getHeaders())
+                .addData("method", request.method())
+                .addData("url", request.getUri())
+                .addData("headers", request.getHeaders())
                 .log();
-        return process(wrappedRequest).whenComplete(
-                (response, failure) -> {
-                    requestTimer.stop();
+        return request.entity().toStrict(30000, _actorSystem)
+                .thenApply(request::withEntity)
+                .thenCompose(strictRequest -> {
+                    final CompletionStage<HttpResponse> process = this.process(strictRequest);
+                    process.whenComplete(
+                            (response, failure) -> {
+                                requestTimer.stop();
 
-                    final int responseStatus;
-                    if (response != null) {
-                        responseStatus = response.status().intValue();
-                    } else {
-                        // TODO(ville): Figure out how to intercept post-exception mapping.
-                        responseStatus = 599;
-                    }
+                                final int responseStatus;
+                                if (response != null) {
+                                    responseStatus = response.status().intValue();
+                                } else {
+                                    // TODO(ville): Figure out how to intercept post-exception mapping.
+                                    responseStatus = 599;
+                                }
 
-                    _metrics.recordTimer(
-                            createMetricName(wrappedRequest, responseStatus, REQUEST_METRIC),
-                            requestTimer.elapsed(TimeUnit.NANOSECONDS),
-                            Optional.of(TimeUnit.NANOSECONDS));
-                    _metrics.recordGauge(
-                            createMetricName(wrappedRequest, responseStatus, BODY_SIZE_METRIC),
-                            entityWrapper.getBytesConsumed());
-                    final int responseStatusClass = responseStatus / 100;
-                    for (final int i : STATUS_CLASSES) {
-                        _metrics.recordCounter(
-                                createMetricName(wrappedRequest, responseStatus, String.format("%s/%dxx", STATUS_METRIC, i)),
-                                responseStatusClass == i ? 1 : 0);
-                    }
+                                _metrics.recordTimer(
+                                        createMetricName(strictRequest, responseStatus, REQUEST_METRIC),
+                                        requestTimer.elapsed(TimeUnit.NANOSECONDS),
+                                        Optional.of(TimeUnit.NANOSECONDS));
+                                _metrics.recordGauge(
+                                        createMetricName(strictRequest, responseStatus, BODY_SIZE_METRIC),
+                                        strictRequest.entity().getContentLengthOption().orElse(0));
+                                final int responseStatusClass = responseStatus / 100;
+                                for (final int i : STATUS_CLASSES) {
+                                    _metrics.recordCounter(
+                                            createMetricName(strictRequest, responseStatus, String.format("%s/%dxx", STATUS_METRIC, i)),
+                                            responseStatusClass == i ? 1 : 0);
+                                }
 
-                    final LogBuilder log;
-                    if (failure != null || responseStatusClass == 5) {
-                        log = LOGGER.info().setEvent("http.in.failure");
-                        if (failure != null) {
-                            log.setThrowable(failure);
-                        }
-                        if (!LOGGER.isTraceEnabled() && LOGGER.isInfoEnabled()) {
-                            log.addData("method", wrappedRequest.method().toString())
-                                    .addData("url", wrappedRequest.getUri().toString())
-                                    .addData(
-                                            "headers",
-                                            StreamSupport.stream(wrappedRequest.getHeaders().spliterator(), false)
-                                                    .map(h -> h.name() + "=" + h.value())
-                                                    .collect(Collectors.toList()));
-                        }
-                    } else {
-                        log = LOGGER.trace().setEvent("http.in.complete");
-                    }
-                    log.addContext("requestId", requestId)
-                            .addData("status", responseStatus)
-                            .log();
+                                final LogBuilder log;
+                                if (failure != null || responseStatusClass == 5) {
+                                    log = LOGGER.info().setEvent("http.in.failure");
+                                    if (failure != null) {
+                                        log.setThrowable(failure);
+                                    }
+                                    if (!LOGGER.isTraceEnabled() && LOGGER.isInfoEnabled()) {
+                                        log.addData("method", strictRequest.method().toString())
+                                                .addData("url", strictRequest.getUri().toString())
+                                                .addData(
+                                                        "headers",
+                                                        StreamSupport.stream(strictRequest.getHeaders().spliterator(), false)
+                                                                .map(h -> h.name() + "=" + h.value())
+                                                                .collect(Collectors.toList()));
+                                    }
+                                } else {
+                                    log = LOGGER.trace().setEvent("http.in.complete");
+                                }
+                                log.addContext("requestId", requestId)
+                                        .addData("status", responseStatus)
+                                        .log();
+                            });
+                    return process;
                 });
     }
 
