899d53cb0080196a9cc7ed783e93bd9948aeffc2
Brandon Arp <brandonarp@gmail.com>
2024-05-30 17:51:43 -0700

    Add metrics to Statsd source (#452)
    * Add metrics to Statsd source

--- src/main/java/com/arpnetworking/metrics/common/sources/StatsdSource.java
+++ src/main/java/com/arpnetworking/metrics/common/sources/StatsdSource.java
@@ -17,10 +17,15 @@ package com.arpnetworking.metrics.common.sources;
 
 import com.arpnetworking.metrics.common.parsers.Parser;
 import com.arpnetworking.metrics.common.parsers.exceptions.ParsingException;
+import com.arpnetworking.metrics.incubator.PeriodicMetrics;
+import com.arpnetworking.metrics.mad.model.Metric;
 import com.arpnetworking.metrics.mad.model.Record;
+import com.arpnetworking.metrics.mad.model.statistics.StatisticFactory;
 import com.arpnetworking.metrics.mad.parsers.StatsdToRecordParser;
 import com.arpnetworking.steno.Logger;
 import com.arpnetworking.steno.LoggerFactory;
+import com.arpnetworking.tsdcore.model.CalculatedValue;
+import com.fasterxml.jackson.annotation.JacksonInject;
 import net.sf.oval.constraint.NotEmpty;
 import net.sf.oval.constraint.NotNull;
 import net.sf.oval.constraint.Range;
@@ -34,6 +39,7 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Source that uses Statsd as input.
@@ -56,10 +62,12 @@ public final class StatsdSource extends ActorSource {
         super(builder);
         _host = builder._host;
         _port = builder._port;
+        _periodicMetrics = builder._periodicMetrics;
     }
 
     private final String _host;
     private final int _port;
+    private final PeriodicMetrics _periodicMetrics;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(StatsdSource.class);
     private static final Parser<List<Record>, ByteBuffer> PARSER = new StatsdToRecordParser();
@@ -80,7 +88,33 @@ public final class StatsdSource extends ActorSource {
          * @return A new {@link Props}
          */
         /* package private */ static Props props(final StatsdSource source) {
-            return Props.create(Actor.class, source);
+            return Props.create(Actor.class, () -> new Actor(source));
+        }
+
+
+        @Override
+        public void preStart() throws Exception, Exception {
+            super.preStart();
+
+            final ActorRef udpManager = Udp.get(getContext().system()).getManager();
+            udpManager.tell(
+                    UdpMessage.bind(getSelf(), new InetSocketAddress(_host, _port)),
+                    getSelf());
+
+            _periodicMetrics.registerPolledMetric(m -> {
+                // TODO(vkoskela): There needs to be a way to deregister these callbacks
+                // This is not an immediate issue since new Aggregator instances are
+                // only created when pipelines are reloaded. To avoid recording values
+                // for dead pipelines this explicitly avoids recording zeroes.
+                final long samples = _receivedSamples.getAndSet(0);
+                final long packets = _receivedPackets.getAndSet(0);
+                if (samples > 0) {
+                    m.recordCounter(String.format("sources/statsd/%s/metric_samples", _metricSafeName), samples);
+                }
+                if (packets > 0) {
+                    m.recordCounter(String.format("sources/statsd/%s/packets", _metricSafeName), packets);
+                }
+            });
         }
 
         @Override
@@ -128,6 +162,23 @@ public final class StatsdSource extends ActorSource {
                 // NOTE: The parsing occurs in the actor itself which can become a bottleneck
                 // if there are more records to be parsed then a single thread can handle.
                 final List<Record> records = PARSER.parse(updReceived.data().toByteBuffer());
+                long samples = 0;
+                for (final Record record : records) {
+                    for (final Metric metric : record.getMetrics().values()) {
+                        samples += metric.getValues().size();
+                        final List<CalculatedValue<?>> countStatistic =
+                                metric.getStatistics().get(STATISTIC_FACTORY.getStatistic("count"));
+                        if (countStatistic != null) {
+                            samples += countStatistic.stream()
+                                    .map(s -> s.getValue().getValue())
+                                    .reduce(Double::sum)
+                                    .map(Double::longValue)
+                                    .orElse(0L);
+                        }
+                    }
+                }
+                _receivedSamples.addAndGet(samples);
+                _receivedPackets.addAndGet(1);
                 records.forEach(_sink::notify);
             } catch (final ParsingException e) {
                 BAD_REQUEST_LOGGER.warn()
@@ -147,11 +198,8 @@ public final class StatsdSource extends ActorSource {
             _sink = source;
             _host = source._host;
             _port = source._port;
-
-            final ActorRef udpManager = Udp.get(getContext().system()).getManager();
-            udpManager.tell(
-                    UdpMessage.bind(getSelf(), new InetSocketAddress(_host, _port)),
-                    getSelf());
+            _periodicMetrics = source._periodicMetrics;
+            _metricSafeName = source.getMetricSafeName();
         }
 
         private boolean _isReady = false;
@@ -163,6 +211,11 @@ public final class StatsdSource extends ActorSource {
         private static final String IS_READY = "IsReady";
         private static final Logger BAD_REQUEST_LOGGER =
                 LoggerFactory.getRateLimitLogger(StatsdSource.class, Duration.ofSeconds(30));
+        private static final StatisticFactory STATISTIC_FACTORY = new StatisticFactory();
+        private final AtomicLong _receivedSamples = new AtomicLong(0);
+        private final AtomicLong _receivedPackets = new AtomicLong(0);
+        private final PeriodicMetrics _periodicMetrics;
+        private final String _metricSafeName;
     }
 
     /**
@@ -203,6 +256,17 @@ public final class StatsdSource extends ActorSource {
             return self();
         }
 
+        /**
+         * Sets the periodic metrics instance.
+         *
+         * @param value The periodic metrics.
+         * @return This instance of {@link HttpSource.Builder}
+         */
+        public Builder setPeriodicMetrics(final PeriodicMetrics value) {
+            _periodicMetrics = value;
+            return self();
+        }
+
         @Override
         protected Builder self() {
             return this;
@@ -214,5 +278,8 @@ public final class StatsdSource extends ActorSource {
         @NotNull
         @Range(min = 1, max = 65535)
         private Integer _port = 8125;
+        @NotNull
+        @JacksonInject
+        private PeriodicMetrics _periodicMetrics;
     }
 }
